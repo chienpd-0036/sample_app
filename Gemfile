4a8b7a4ccfa67009bfc99b0cb14962de38fae647
chienk58 <pham.dinh.chien@sun-asterisk.com>
2019-07-05 10:43:48 +0700

    Chapter 8: Basic Login

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "bootstrap-sass", "3.3.7"
 gem "coffee-rails", "~> 4.2"
 gem "config"
 gem "jbuilder", "~> 2.5"
+gem "jquery-rails"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.3"
 gem "rubocop", "~> 0.54.0", require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,15 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.check_user.pass_min_length}
 
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine.MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
   private
 
   def downcase_email
